837206eed040f85d780eb16e2838c05a700a2e1b
hartkkuh <a217287499@gmail.com>
2026-07-28 08:42:15 +0300

    commit 7

--- app.py
+++ app.py
@@ -6,17 +6,22 @@ from browser import get_browser
 app = Flask(__name__)
 
 
-@app.route("/open", methods=["GET"])
-def open_get():
-    url = request.args.get("url")
+@app.route("/open", methods=["POST"])
+def home():
+    data = request.get_json(silent=True) or {}
+    url = data.get("url")
     if not url:
         return jsonify({"error": "URL is required"}), 400
 
     browser = get_browser()
-    browser.open(url)
+    try:
+        browser.open(url)
+    except PlaywrightTimeoutError:
+        return jsonify({"error": f"Timeout loading {url}"}), 504
 
     image = browser.img()
     return Response(image, mimetype="image/png")
+
 
 if __name__ == "__main__":
     app.run(debug=True, threaded=True)
